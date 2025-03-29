847a7ded72d9f833665d838bf86cae8df3407296
Andrew Birmingham <andrew_birmingham@msn.com>
2025-03-28 23:25:59 -0600

    most of query done for typeDefs

--- server/src/schemas/typeDefs.ts
+++ server/src/schemas/typeDefs.ts
@@ -1,3 +1,31 @@
-const typeDefs = ``;
+const typeDefs = `
+    type User {
+    _id: ID!
+    username: String!
+    email: String!
+    bookCount: Int
+    savedBooks: [Book]
+    }
+
+    type Book {
+    bookId: ID!
+    authors: [Sting!]
+    description: String!
+    title: String!
+    image:
+    link:
+    }
+
+    type Query {
+
+    user: User
+    me: User
+    }
+    type Auth {
+    token: ID!
+    user: User
+    }
+    
+`;
 
 export default typeDefs;
