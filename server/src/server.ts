b50c2939e36bb65a0a942c56dae6f47378e70227
Andrew Birmingham <andrew_birmingham@msn.com>
2025-03-28 23:09:26 -0600

    updated server.ts with apollo server

---
+++ server/src/server.ts
@@ -0,0 +1,41 @@
+import express from 'express';
+import path from 'node:path';
+import db from './config/connection.js';
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
+import { typeDefs, resolvers } from 'tbd'; 
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const startApolloServer = async () => {
+  await server.start();
+  
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+  
+  app.use('/graphql', expressMiddleware(server));
+
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/dist')));
+
+    app.get('*', (_req, res) => {
+      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    });
+  }
+
+  db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+  });
+};
+
+startApolloServer();
+
