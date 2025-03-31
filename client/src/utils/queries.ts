cc2328244e96c279d46697381031e38e90543e52
Andrew Birmingham <andrew_birmingham@msn.com>
2025-03-30 18:12:17 -0600

    updated package.json files

---
+++ client/src/utils/queries.ts
@@ -0,0 +1,15 @@
+import { gql } from '@apollo/client';
+
+export const GET_ME = gql`
+  query me($username: String!) {
+    user(username: $username) {
+      _id
+      username
+      email
+      saveBooks {
+        _id
+        bookCount
+        
+      }
+    }
+  }
