3c0f40c5004faa5ab1b8c2ccf6f1d911243e2cb8
Andrew Birmingham <andrew_birmingham@msn.com>
2025-04-01 21:03:46 -0600

    added authenticatetoken to my graphql exppress middleware in server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,6 +5,7 @@ import db from './config/connection.js';
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
 import { typeDefs, resolvers } from './schemas/index.js'; 
+import { authenticateToken } from './utils/auth.js';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -21,8 +22,11 @@ const startApolloServer = async () => {
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
   
-  app.use('/graphql', expressMiddleware(server as any));
-
+  app.use('/graphql', expressMiddleware(server as any,
+    {
+      context: authenticateToken as any
+    }
+  ));
   if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/dist')));
 
